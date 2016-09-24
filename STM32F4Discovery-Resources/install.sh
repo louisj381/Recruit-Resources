91ad7ed29f1a5d5e656e72f6fd12dde627d37659
Enoch Tsang <etsang1@hotmail.com>
2016-09-24 17:04:00 -0600

    add install script for stm32f4discovery tools

---
+++ STM32F4Discovery-Resources/install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+sudo apt-get install build-essential git libsane:i386 ia32-libs-multiarch autoconf libusb-1.0-0-dev pkg-config
+
+# Install arm compiler
+command -v arm-none-eabi-gcc >/dev/null 2>&1 || {
+    git clone https://github.com/adamgreen/gcc4mbed /opt/gcc4mbed
+    (cd /opt/gcc4mbed && \
+        chmod +x linux_install && \
+        sed -i '108d;109d;110d;134d' linux_install && \ # Remove 'press any key to continue' and building samples
+        ./linux_install)
+   echo "export PATH=$PATH:/opt/gcc4mbed/gcc-arm-none-eabi/bin/" >> ~/.profile
+}
+
+# Install CubeMX
+mkdir cubemx
+cp auto-install.xml cubemx
+(cd cubemx && \
+    wget https://s3-us-west-2.amazonaws.com/ucsolarteam.hostedfiles/en.stm32cubemx.zip && \
+    unzip en.stm32cubemx.zip && \
+    chmod +x SetupSTM32CubeMX-4.16.1.linux && \
+    ./SetupSTM32CubeMX-4.16.1.linux auto-install.xml)
+rm -r cubemx
+
+# Install STLink
+git clone https://github.com/texane/stlink.git --depth 1
+mkdir stlink/build
+(cd stlink/build && cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j4)
+sudo cp stlink/build/st-flash /usr/local/bin/st-flash
+sudo cp stlink/build/st-info /usr/local/bin/st-info
+sudo cp stlink/build/srcgdbserver/st-util /usr/local/bin/st-util
+rm stlink -r
+
+# Install CubeMX2Makefile
+sudo git clone https://github.com/baoshi/CubeMX2Makefile.git /opt/CubeMX2Makefile
+sudo ln -s /opt/CubeMX2Makefile/CubeMX2Makefile.py /opt/CubeMX2Makefile/CubeMX2Makefile
+echo "export PATH=\$PATH:/opt/CubeMX2Makefile" >> ~/.profile
+
+# source PATH changes
+source  ~/.profile
