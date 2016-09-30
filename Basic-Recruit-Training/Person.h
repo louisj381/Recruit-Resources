1dc801487af680e7d9ecc99ad27f346439872803
bill-luu <billluu619@gmail.com>
2016-09-30 15:47:17 -0600

    Continued work on PR comments

--- Basic-Recruit-Training/Person.h
+++ Basic-Recruit-Training/Person.h
@@ -1,29 +1,29 @@
-//This line is a preprocessor directive that prevents this header file from accidentally being included multiple times by source files.
+// This line is a preprocessor directive that prevents this header file being compiled multiple times by source files. It should be included in each header file.
 #pragma once
 
 #include <string>
 class Person
 {
 public:
-//A Rule of Thumb when designing classes is to have a default constructor, regular constructor, and a destructor.
+// A Rule of Thumb when designing classes is to have a default constructor, regular constructor, and a destructor.
 	Person();
-	Person(const std::string name, int age);
+	Person(const std::string& name, int age);
 	~Person();
 
 	int getAge() const;
-	std::string getName() const;
+	const std::string& getName() const;
 
 	void printInfo();
 
-	void setName(const std::string newName);
+	void setName(const std::string& newName);
 	void setAge(int newAge);
-//Print the combined Age of everyone pointed to by x
-	static int combinedAge(Person** x, int size);
+
+	static int combinedAge(Person** x, int size); // Print the combined Age of everyone pointed to by x
 	static void birthday(Person& x);
 private:
 	std::string name_;
-//age_ should be a dynamically allocated.
-	int* age_;
+
+	int* age_; // age_ should be a dynamically allocated.
 
 };
 
--- Basic-Recruit-Training/main.cpp
+++ Basic-Recruit-Training/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //Imports cout
+#include <iostream> // Imports cout
 #include <sstream>
 #include <string>
 #include "Person.h"
@@ -7,40 +7,26 @@ void compareAnswers(const std::string myAnswer, const std::string correctAnswer)
 {
     if(myAnswer.compare(correctAnswer) != 0)
     {
-        std::cout << "Incorrect." << std::endl;
-        std::cout << "Expected: " << correctAnswer << std::endl;
-        std::cout << "You got: " << myAnswer << std::endl;
+        std::cout << "Incorrect." << std::endl
+                  << "Expected: " << correctAnswer << std::endl
+                  << "You got: " << myAnswer << std::endl;
     }
     else
     {
         std::cout << "Correct." << std::endl;
     }
 }
-//Swaps a & b.
+// Swaps a & b.
 void mySwap(int& a, int& b)
 {
 }
-//Using y++ or similar variants is not allowed for this function.
+
 void myIncrement(int* x)
 {
 }
 
-int main()
+void personFunctions()
 {
-    int a = 5;
-    int b = 6;
-    int* c = 0;
-    //Implement mySwap() on a & b.
-
-    //Implement myIncrement() to Increment a.
-
-    std::stringstream swapIncrementAnswer;
-    std::cout << "Checking Swap and Increment: " << std::endl;
-    swapIncrementAnswer << "a is " << a << ", b is " << b;
-    std::cout << swapIncrementAnswer.str() << std::endl;
-    const std::string correctSwapIncrementAnswer = "a is 7, b is 5";
-    compareAnswers(swapIncrementAnswer.str(), correctSwapIncrementAnswer);
-
     Person Bill;
     Bill.setName("Bill");
     Bill.setAge(18);
@@ -48,16 +34,18 @@ int main()
 
     Person Bob("Bob", 30);
     Bob.printInfo();
+
     Person* Joe = new Person("Joe", 15);
     Joe->printInfo();
 
     Person* personArray[] = {&Bill, &Bob, Joe};
+
     std::stringstream myCombinedAgeAnswer;
     myCombinedAgeAnswer << "The combined age of Bill, Bob, and Joe is: " << Person::combinedAge(personArray, sizeof(personArray)/sizeof(Person*));
     const std::string correctCombinedAgeAnswer = "The combined age of Bill, Bob, and Joe is: 63";
 
-    std::cout << "Checking Person::combinedAge:" << std::endl;
-    std::cout << myCombinedAgeAnswer.str() << std::endl;
+    std::cout << "Checking Person::combinedAge:" << std::endl
+              << myCombinedAgeAnswer.str() << std::endl;
     compareAnswers(myCombinedAgeAnswer.str(), correctCombinedAgeAnswer);
 
     Person::birthday(Bill);
@@ -66,9 +54,31 @@ int main()
     myBirthdayAnswer << "It was Bill's Birthday, he is now " << Bill.getAge();
     const std::string correctBirthdayAnswer = "It was Bill's Birthday, he is now 19";
 
-    std::cout << "Checking Person::Birthday:" << std::endl;
-    std::cout << myBirthdayAnswer.str() << std::endl;
+    std::cout << "Checking Person::Birthday:" << std::endl
+              << myBirthdayAnswer.str() << std::endl;
     compareAnswers(myBirthdayAnswer.str(), correctBirthdayAnswer);
+    delete Joe;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int* c = 0;
+
+    std::stringstream swapIncrementAnswer;
+    std::cout << "Checking Swap and Increment: " << std::endl;
+    swapIncrementAnswer << "a is " << a << ", b is " << b;
+    std::cout << swapIncrementAnswer.str() << std::endl;
+    const std::string correctSwapIncrementAnswer = "a is 7, b is 5";
+    compareAnswers(swapIncrementAnswer.str(), correctSwapIncrementAnswer);
+
+// Pass a & b into mySwap
+
+// Pass in a to myIncrement
+
+// Uncomment this once you have completed the definitions of the Person class.
+  //personFunctions();
 
     return 0;
 }
