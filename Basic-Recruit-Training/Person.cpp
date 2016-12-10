9578542a979fe9faadd14b4b01e33e9297cd370b
Louis Johnson <louisj381@gmail.com>
2016-12-10 10:17:19 -0700

    Updated files according to comments

---
+++ Basic-Recruit-Training/Person.cpp
@@ -0,0 +1,54 @@
+#include "Person.h"
+
+Person::Person()    {
+name_ = "";
+age_ = new int;
+age_ = 0;
+}
+
+Person::Person(const std::string& name, int a)   {
+ name_ = name;
+ age_ = new int(a);
+ }
+
+Person::~Person()  {
+    delete(age_);
+ }
+
+int Person::getAge() const {
+    return *age_;
+ }
+
+const std::string& Person::getName() const  {
+    return name_;
+}
+
+void Person::printInfo() const   {
+ std::cout << "Name: " << getName() << std::endl
+              << "Age: " << getAge() << std::endl;
+
+}
+
+void Person::setName(const std::string& newName)  {
+    name_ = newName;
+}
+void Person::setAge(int newAge)   {
+    age_ = 0;
+    age_ = new int(newAge);
+
+}
+
+void Person::birthday(Person& x)   {
+    x.setAge (x.getAge() + 1);
+ }
+
+int Person::combinedAge(Person** x, int size)  {
+ int c = 0; //
+ for (int index = 0; index < size; index++)  {
+    c = c + (x[index])->getAge();   //have to use brackets otherwise dot operator takes precesdence over double pointer
+    std::cout << "combinedAge: " << c << " age: " << (x[index])->getAge() << "\n";
+ }
+
+return c;
+}
+
--- Basic-Recruit-Training/Person.h
+++ Basic-Recruit-Training/Person.h
@@ -1,20 +1,21 @@
 // This line is a preprocessor directive that this header is only defined once.
 // This needs to be included in each header file.
 #pragma once
-
+#include <iostream> //Imports cout
+#include <sstream>
 #include <string>
 class Person
 {
-public:
+public: //declaring functions here
 // A Rule of Thumb when designing classes is to have a default constructor, regular constructor, and a destructor.
-    Person();
-    Person(const std::string& name, int age);
-    ~Person();
+    Person();   //default Constructor
+    Person(const std::string& name, int age);   //regular
+    ~Person();  //destructor
 
     int getAge() const;
     const std::string& getName() const;
 
-    void printInfo();//Print out the name and age of the person.
+    void printInfo() const;//Print out the name and age of the person.
 
     void setName(const std::string& newName);
     void setAge(int newAge);
@@ -24,7 +25,7 @@ public:
 private:
     std::string name_;
 
-    int* age_; // age_ should be a dynamically allocated.
+    int* age_; // age_ should be a dynamically allocated. here, the pointer is declared
 
 };
 
